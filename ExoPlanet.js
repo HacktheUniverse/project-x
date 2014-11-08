b8957ea2dc14cc1eb3ca5817fcb7f07d54fe2288
Joseph Giralt <Joe.m.giralt@gmail.com>
2014-11-08 11:55:02 -0500

    more broken angulaer

---
+++ ExoPlanet.js
@@ -0,0 +1,34 @@
+
+    "use strict";
+    var app = angular.module('ExoPlanet', []);
+
+// var removeExtraSpaces = function(str){
+//     // Turns multiple spaces to one space using regex. 'hi  there   !' = 'hi there !'
+//     return str.replace(/ +/g, " ");
+// };
+
+// var removeCommas = function(str) {
+//     // Removes all commas from string.
+//     return str.replace(',','');
+// };
+
+// var isNumber = function(input){
+//     return !isNaN(Number(input));
+// };
+
+// var capitalizeFirstLetter = function(string){
+//     if (string === '') return '';
+//     return string.charAt(0).toUpperCase() + string.slice(1);
+// };
+
+// var rounded = function(num, numDecimalPlaces){
+//     if(typeof numDecimalPlaces === "undefined") numDecimalPlaces = 2;
+//     var temp = Math.pow(10, numDecimalPlaces);
+//     return Math.round(num * temp) / temp;
+// };
+
+    app.controller('ExoCtrl', ['$scope', function($scope){ 
+        $scope.systems = [{'planet': 'mars'}];
+    }]);
+
+
